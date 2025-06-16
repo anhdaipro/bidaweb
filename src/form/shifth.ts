6e382cc3c3093194c2d69f7d6171dec227695c35
Pham Dai <daip6690@gmail.com>
2025-06-16 23:04:25 +0700

    first commit

--- src/form/shifth.ts
+++ src/form/shifth.ts
@@ -4,20 +4,18 @@ export const STATUS_LABEL: Record<number, string> = {
 [STATUS_ACTIVE]: 'Hoạt động',
 [STATUS_INACTIVE]: 'Không hoạt động',
 }
-export enum SHIFT_TYPES {
-    MORNING= 1,
-    AFTERNOON= 2,
-    NIGHT= 3,
-    ADMINISTRATIVE = 4,
-};
-export const SHIFT_LABELS: Record<SHIFT_TYPES, string> = {
-    [SHIFT_TYPES.MORNING]: 'Ca sáng 08:00 - 12:00',
-    [SHIFT_TYPES.AFTERNOON]: 'Ca chiều 13:00 - 17:00',
-    [SHIFT_TYPES.NIGHT]: 'Ca tối 18:00 - 22:00',
-    [SHIFT_TYPES.ADMINISTRATIVE]: 'Ca hành chính 08:00 - 17:00',
+const MORNING= 1
+   const AFTERNOON= 2
+    const NIGHT= 3
+    const ADMINISTRATIVE = 4
+export const SHIFT_LABELS: Record<number, string> = {
+    [MORNING]: 'Ca sáng 08:00 - 12:00',
+    [AFTERNOON]: 'Ca chiều 13:00 - 17:00',
+    [NIGHT]: 'Ca tối 18:00 - 22:00',
+    [ADMINISTRATIVE]: 'Ca hành chính 08:00 - 17:00',
 };
 export const SHIFT_RANDOM =  [
-    SHIFT_TYPES.MORNING,
-    SHIFT_TYPES.AFTERNOON,
-    SHIFT_TYPES.NIGHT,
+   MORNING,
+AFTERNOON,
+    NIGHT,
 ]
